0319f377d7b0e5f27934ed071c28fbe79e36e17b
shirokovnv <shirokovnv@gmail.com>
2024-08-28 12:55:21 +0500

    Add documents model, migration and factory

---
+++ app/Models/Document.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\DocumentType;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Document extends Model
+{
+    use HasFactory;
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'type' => DocumentType::class,
+        ];
+    }
+}
---
+++ database/factories/DocumentFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Enums\DocumentType;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Foundation\Testing\WithFaker;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Document>
+ */
+class DocumentFactory extends Factory
+{
+    use WithFaker;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'type' => $this->faker->randomElement(DocumentType::values()),
+        ];
+    }
+}
